eac9de9bccd4c98e4cdddca9b789ef488dd4ee9e
Denis Lindner <lindnerdenis19@gmail.com>
2025-09-01 09:56:26 -0300

    v0.1.0 Adição de Todas as Entidades e de todos os DTO do Sistema

---
+++ API-Sistema-Liga-Nacional-de-Futebol/src/main/java/com/denis/API/Sistema/Liga/Nacional/de/Futebol/model/dto/EmpresaResponse.java
@@ -0,0 +1,4 @@
+package com.denis.API.Sistema.Liga.Nacional.de.Futebol.model.dto;
+
+public record EmpresaResponse() {
+}
---
+++ API-Sistema-Liga-Nacional-de-Futebol/src/main/java/com/denis/API/Sistema/Liga/Nacional/de/Futebol/model/entity/Partida.java
@@ -0,0 +1,4 @@
+package com.denis.API.Sistema.Liga.Nacional.de.Futebol.model.entity;
+
+public class Partida {
+}
